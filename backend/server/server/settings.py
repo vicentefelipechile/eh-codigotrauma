3ad9f9fbcafe7b047bde0ec3951d56e04d530c89
Vicente <vicentefelipechile@gmail.com>
2023-09-12 16:55:28 -0300

    commit

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -126,7 +126,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '../../frontend'
+STATICFILES_DIRS = [
+    BASE_DIR / '../../frontend'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
