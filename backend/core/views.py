a8560210211823bf8d9a44c4432c1a1d9ef1f74d
Duoc <roci.palominos@duocuc.cl>
2023-09-26 17:07:42 -0300

    Update views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,9 +6,11 @@ DOMAIN_NAME: str = settings.DOMAIN_NAME
 
 # Create your views here.
 def home(request: WSGIRequest) -> HttpResponse:
+    Respuesta: str = f"""
+        <h1>hello world</h1>
+        <br>
+        <h2>La aplicacion se llamada {DOMAIN_NAME}</h2>
+        <h2>Y tu IP es {request.get_host()}</h2>
+    """
 
-    return HttpResponse(f"""
-                        <h1>hello world</h1>
-                        <br>
-                        <h2>La aplicacion se llamada {DOMAIN_NAME}</h2>
-                        <h2>Y tu IP es {request.META['REMOTE_ADDR']}</h2>""")
+    return HttpResponse(Respuesta)
