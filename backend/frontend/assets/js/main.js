426a20f51422999c179b26fa821cec8751dbe82b
Vicente <vicentefelipechile@gmail.com>
2023-10-03 15:35:52 -0300

    completo reset

--- backend/frontend/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-/*  Show/Hidden popup login */
-function show_popup_login(){
-	let popup = document.querySelector(".popup-login");
-	if(popup.classList.contains('active')){
-		popup.classList.remove('active');
-	}else{
-		popup.classList.add('active');
-	}
-}
