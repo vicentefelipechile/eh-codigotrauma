b116a2be378b5ea83821f35d3e8c5d7883c28bf1
Vicente <vicentefelipechile@gmail.com>
2023-10-03 15:53:47 -0300

    cambio completado

--- root/__backend__.py
+++ root/__backend__.py
@@ -5,13 +5,16 @@
 import os
 import sys
 import json
+import pathlib
 
 # ============================================================
 # Obtener la configuracion del proyecto en "./config.json"
 # ============================================================
 
+MAIN_PATH: str = str(pathlib.Path(__file__).parent.parent.absolute()) + "/"
+
 try:
-    with open('./config.json', 'r') as Configuracion:
+    with open(MAIN_PATH + './config.json', 'r') as Configuracion:
         Configuracion: dict = json.load(Configuracion)
 except FileNotFoundError:
     print("=====================================")
@@ -30,18 +33,12 @@ try:
 except ImportError:
     print("=====================================")
     print("No se ha encontrado Django instalado")
-    print("=====================================")
-
-    # Intentar instalar django
-    print("=====================================")
     print("Instalando Django...")
     print("=====================================")
 
-    import os
-    import sys
-
-    print(f"{sys.executable} -m pip install django")
-    os.system(f'{sys.executable} -m pip install django')
+    # Nuevo formato utilizando MAIN_PATH
+    print(f"{sys.executable} -m pip install -r {MAIN_PATH}requirements.txt")
+    os.system(f'{sys.executable} -m pip install -r {MAIN_PATH}requirements.txt')
 
 print("=====================================")
 print("Caracteristicas necesarias instaladas")
@@ -51,27 +48,27 @@ print("=====================================")
 
 try:
     # Migrar aplicaciones
-    print(f'python {Configuracion["backend"]["MAIN_PATH"]}manage.py migrate')
-    os.system(f'python {Configuracion["backend"]["MAIN_PATH"]}manage.py migrate')
+
+    # Nuevo formato utilizando MAIN_PATH
+    print(f'python {MAIN_PATH}manage.py migrate')
+    os.system(f'python {MAIN_PATH}manage.py migrate')
 
     if Configuracion["alwaysOpenBrowser"]:
         print(f'python -m webbrowser "http://127.0.0.1:8000"')
         os.system(f'python -m webbrowser "http://127.0.0.1:8000"') 
 
+    print(f'python {MAIN_PATH}manage.py runserver')
 
-    print(f'python {Configuracion["backend"]["MAIN_PATH"]}manage.py runserver')
     print("=====================================")
-    os.system(f'python {Configuracion["backend"]["MAIN_PATH"]}manage.py runserver')
+    os.system(f'python {MAIN_PATH}manage.py runserver')
 except KeyboardInterrupt:
     pass
 except Exception as Error:
     print("=====================================")
     print("Error al iniciar el proyecto")
-    print("=====================================")
     print("")
     print(Error)
     print("")
-    print("=====================================")
     print("Terminando...")
     print("=====================================")
     print("")
