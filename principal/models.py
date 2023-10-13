25d5a15173770f4b6dfd9b33bb49f074b046d075
chiouwur <106972930+chiouwur@users.noreply.github.com>
2023-10-13 18:26:31 -0300

    Update models.py
    uwu

--- principal/models.py
+++ principal/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class RegistroEmergencias(models.Model):
     id = models.CharField(primary_key=True, max_length=8)
-    descripcion = models.TextField( max_length=50)
+    descripcion = models.TextField(max_length=50)
+    codigoColor = models.TextField(max_length=20)
+    fecha = models.DateTimeField(max_length=30)
+    nro_pacientes = models.IntegerField(max_length=5)
+
+    def __str__(self):
+        return self.id
+
+
