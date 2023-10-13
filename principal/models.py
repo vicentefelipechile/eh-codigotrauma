789295df946017cdf53db74d7b92fda79cd5fce5
chiouwur <106972930+chiouwur@users.noreply.github.com>
2023-10-13 18:15:21 -0300

    Update models.py
    Clase registro emergencia1

--- principal/models.py
+++ principal/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class RegistroEmergencias(models.Model):
+    id = models.CharField(primary_key=True, max_length=8)
+    descripcion = models.TextField( max_length=50)
