02ed4710a13b06ac223a17de90f74e18b7dc76e5
Duoc <roci.palominos@duocuc.cl>
2023-10-31 15:39:53 -0300

    Update bdpopulate.py

--- bdpopulate.py
+++ bdpopulate.py
@@ -11,7 +11,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "codigotrauma.settings")
 django.setup()
 
 
-from principal.models import Paciente, RegistroEmergencias, HistorialEmergencias, Administrador, Secretario, DoctorClave, HistorialDoctoresEmergencia
+from principal.models import Paciente, RegistroEmergencias, HistorialEmergencias, Administrador, Secretario, DoctorClave, HistorialDoctoresEmergencia, HoraDia, Horario, DiaSemana, Area
 
 
 from principal.models import Paciente
@@ -115,4 +115,73 @@ def crear_administradores_falsos(cantidad):
 crear_administradores_falsos(100)
 
 def crear_doctoresclave_falsos(cantidad):
-    
+    for _ in range(cantidad):
+         nombre_usuario_ficticio = fake.user_name()
+        
+        
+        
+def crear_horas_dia_falsas(cantidad):
+    for _ in range(cantidad):
+        # Genera horas de inicio y fin ficticias
+        hora_inicio_ficticia = fake.time_object(end_datetime=None)
+        
+        # Asegura que la hora de fin sea posterior a la hora de inicio
+        hora_fin_ficticia = fake.time_object(end_datetime=None)
+        while hora_fin_ficticia <= hora_inicio_ficticia:
+            hora_fin_ficticia = fake.time_object(end_datetime=None)
+        
+        # Crea una instancia de HoraDia
+        hora_dia = HoraDia(
+            HoraInicio=hora_inicio_ficticia,
+            HoraFin=hora_fin_ficticia,
+        )
+        hora_dia.save()
+
+crear_horas_dia_falsas(100)
+
+def crear_dias_semana_falsos(cantidad):
+    for _ in range(cantidad):
+        # Genera un nombre de día ficticio
+        nombre_ficticio = fake.day_of_week()
+        
+        # Crea una instancia de DiaSemana
+        dia_semana = DiaSemana(
+            Nombre=nombre_ficticio,
+        )
+        dia_semana.save()
+
+crear_dias_semana_falsos(100)
+
+def crear_horarios_falsos(cantidad):
+    for _ in range(cantidad):
+        # Genera una referencia ficticia a un día de la semana
+        dia_semana_ficticio = DiaSemana.objects.order_by("?").first()
+        
+        # Genera una referencia ficticia a una hora del día
+        dia_hora_ficticio = HoraDia.objects.order_by("?").first()
+        
+        # Genera una descripción de clase ficticia
+        clase_ficticia = fake.job()
+        
+        # Crea una instancia de Horario
+        horario = Horario(
+            DiaSemana=dia_semana_ficticio,
+            DiaHora=dia_hora_ficticio,
+            Clase=clase_ficticia,
+        )
+        horario.save()
+
+crear_horarios_falsos(100)
+
+def crear_areas_falsas(cantidad):
+    for _ in range(cantidad):
+        # Genera un nombre de área ficticio
+        nombre_area_ficticio = fake.word(ext_word_list=["Cardiología", "Dermatología", "Ginecología", "Neurología", "Ortopedia"])
+        
+        # Crea una instancia de Area
+        area = Area(
+            Nombre=nombre_area_ficticio,
+        )
+        area.save()
+
+crear_areas_falsas(100)
